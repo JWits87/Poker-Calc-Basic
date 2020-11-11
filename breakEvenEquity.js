8bc02934c688c72301a3bab4ddcdab5a9a6ed8d4
Jordan <jordanwstroud@gmail.com>
2020-11-11 12:47:58 -0500

    Added form to BESuccess.html Altered breakEvenEquity Calculations

--- breakEvenEquity.js
+++ breakEvenEquity.js
@@ -1,9 +1,11 @@
 // breakEvenEquity.js - A poker break even equity drill in JavaScript
 // breakEvenEquity generates a random pot size and random bet size and asks for a break even bluff success frequency
 
-let potSize = Math.random(0, 100);
+let potSize = Math.random();
+potSize = Math.floor(potSize * 100)
 let breakEvenBluffSuccess = Math.random();
 let bluffSize = potSize * breakEvenBluffSuccess;
+bluffSize = Math.floor(bluffSize);
+breakEvenBluffSuccess = (bluffSize / (potSize + bluffSize));
 
 console.log("Pot Size: " + potSize + "  Bluff Size: " + bluffSize + "  BreakEven Bluff Success: " + breakEvenBluffSuccess);
- 
